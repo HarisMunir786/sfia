f99d869f1167ea5dddccdd16f4be81d5b2463b7b
HarisMunir786 <Haris.Munir@qa.com>
2020-03-04 15:57:27 +0000

    commited changes on home.html

--- application/models.py
+++ application/models.py
@@ -1,22 +1,22 @@
-#from application import db
+from application import db
 #from application import login_manager
 #from flask_login import UserMixin
 
-#class Books(db.Model):
-#	id = db.Column(db.Integer, primary_key=True)
-#	genre = db.Column(db.String(30), nullable=False)
-#	author = db.Column(db.String(30), nullable=False)
-#	title = db.Column(db.String(100), nullable=False, unique=True)
-#	content = db.Column(db.String(500), nullable=False, unique=True)
+class Books(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	genre = db.Column(db.String(30), nullable=False)
+	author = db.Column(db.String(30), nullable=False)
+	title = db.Column(db.String(100), nullable=False, unique=True)
+	content = db.Column(db.String(500), nullable=False, unique=True)
 #	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-#	def __repr__(self):
-#		return ''.join([
-#			'Genre: ', self.genre, '\r\n',
-#			'Author: ', self.author, '\r\n',
-#			'Title: ', self.title, '\r\n',
-#			'Content: ', self.content
-#			])
+	def __repr__(self):
+		return ''.join([
+			'Genre: ', self.genre, '\r\n',
+			'Author: ', self.author, '\r\n',
+			'Title: ', self.title, '\r\n',
+			'Content: ', self.content
+			])
 
 #class User(db.Model, UserMixin):
 #	id = db.Column(db.Integer, primary_key=True)
